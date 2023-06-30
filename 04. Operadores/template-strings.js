17bed238b89340259c1aaf288df3939a822ca9bf
Fogaca <rafael.fogaca@siemens.com>
2023-06-30 12:13:01 -0300

    final do capítulo 4

---
+++ 04. Operadores/template-strings.js
@@ -0,0 +1,11 @@
+const nome = "Rafael";
+const idade = 2023-2003;
+const cidadeDeNascimento = "Jundiaí";
+
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + 
+// " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome} minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
